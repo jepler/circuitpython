220c64170243df34a1d3669f63e438c577e7b688
Jeff Epler <jepler@gmail.com>
2018-03-31 10:59:34 -0500

    py/stacktrl: Add ifdef guards to gcc-specific code
    (different tricks may be required to fully avoid inlining of these
    functions on other compilers, however)

--- py/stackctrl.c
+++ py/stackctrl.c
@@ -29,7 +29,9 @@
 
 void mp_stack_ctrl_init(void) {
     // Force routine to not be inlined. Better guarantee than MP_NOINLINE for -flto.
+#ifdef __GNUC__
     __asm volatile ("");
+#endif
     volatile int stack_dummy;
     MP_STATE_THREAD(stack_top) = (char*)&stack_dummy;
 }
@@ -41,7 +43,9 @@ void mp_stack_set_top(void *top) {
 mp_uint_t mp_stack_usage(void) {
     // Assumes descending stack
     // Force routine to not be inlined. Better guarantee than MP_NOINLINE for -flto.
+#ifdef __GNUC__
     __asm volatile ("");
+#endif
     volatile int stack_dummy;
     return MP_STATE_THREAD(stack_top) - (char*)&stack_dummy;
 }
