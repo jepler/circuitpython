1e655cb8ed5a0f6557b3ace3b7f24ba4ae6dd241
Jeff Epler <jepler@gmail.com>
2022-01-05 16:10:20 -0600

    WIP

--- shared-module/floppyio/__init__.c
+++ shared-module/floppyio/__init__.c
@@ -25,12 +25,13 @@
  */
 
 #include "shared-bindings/floppyio/__init__.h"
+#include "common-hal/floppyio/__init__.h"
 #include "shared-bindings/digitalio/DigitalInOut.h"
 
 #include <string.h>
 
 __attribute__((optimize("O3")))
-int common_hal_floppyio_track_readinto(void *buf, size_t len, digitalio_digitalinout_obj_t *data, digitalio_digitalinout_obj_t *index) {
+int common_hal_floppyio_flux_readinto(void *buf, size_t len, digitalio_digitalinout_obj_t *data, digitalio_digitalinout_obj_t *index) {
     uint32_t index_mask;
     volatile uint32_t *index_port = common_hal_digitalio_digitalinout_get_reg(data, DIGITALINOUT_REG_READ, &index_mask);
 
@@ -85,4 +86,88 @@ int common_hal_floppyio_track_readinto(void *buf, size_t len, digitalio_digitali
     common_hal_mcu_enable_interrupts();
 
     return pulses_ptr - pulses;
+}
+
+__attribute__((optimize("O3")))
+int common_hal_floppyio_rawbit_readinto(void *buf, size_t len, digitalio_digitalinout_obj_t *data_, digitalio_digitalinout_obj_t *index) {
+    uint32_t index_mask;
+    volatile uint32_t *index_port = common_hal_digitalio_digitalinout_get_reg(index, DIGITALINOUT_REG_READ, &index_mask);
+
+    uint32_t data_mask;
+    volatile uint32_t *data_port = common_hal_digitalio_digitalinout_get_reg(data_, DIGITALINOUT_REG_READ, &data_mask);
+
+#define READ_INDEX() ((*index_port & index_mask))
+#define READ_DATA() ((*data_port & data_mask))
+
+    memset(buf, 0, len);
+
+#define T2_5 (FLOPPYIO_SAMPLERATE * 5 / 2 / 1000000)
+#define T3_5 (FLOPPYIO_SAMPLERATE * 7 / 2 / 1000000)
+    uint32_t *data_start = buf, *data_ptr = data_start, *data_end = data_start + len / sizeof(*data_ptr);
+
+    common_hal_mcu_disable_interrupts();
+
+    // wait for index pulse low
+    while (READ_INDEX()) { /* NOTHING */
+    }
+
+    bool last_index_state = READ_INDEX();
+    unsigned index_transitions = 0;
+
+    // if data line is low, wait till it rises
+    while (!READ_DATA()) { /* NOTHING */
+    }
+
+    // and then till it drops down
+    while (READ_DATA()) { /* NOTHING */
+    }
+
+    uint32_t data = 0, bitpos = 0;
+
+#define PUT_BIT(x) do { \
+        data = (data << 1) | x; \
+        if (++bitpos % 32 == 0) { \
+            *data_ptr++ = data; \
+            if (data_ptr == data_end) { \
+                break; \
+            } \
+        } \
+} while (0)
+
+    while (data_ptr < data_end) {
+        bool index_state = READ_INDEX();
+        if (!last_index_state && index_state) {
+            index_transitions++;
+            if (index_transitions == 2) {
+                break;
+            }
+        }
+        last_index_state = index_state;
+
+        PUT_BIT(1);
+        PUT_BIT(0);
+
+        while (!READ_DATA()) {
+        }
+
+        int pulse_count = 0;
+        while (READ_DATA() && pulse_count < T2_5) {
+            pulse_count++;
+        }
+        if (pulse_count < T2_5) {
+            continue;
+        }
+        PUT_BIT(0);
+
+        while (READ_DATA() && pulse_count < T3_5) {
+            pulse_count++;
+        }
+        if (pulse_count < T3_5) {
+            continue;
+        }
+        PUT_BIT(0);
+    }
+    common_hal_mcu_enable_interrupts();
+
+    return (data_ptr - data_start) * sizeof(*data_ptr);
 }
